b25693836eaa761961253b08cbb57a38a9cc245f
Scaunasu Lucica <scaunasu_lucicaelena@yahoo.com>
2024-03-27 22:30:53 +0200

    add files

--- beautysalon/appointments/urls.py
+++ beautysalon/appointments/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -8,7 +9,15 @@ urlpatterns = [
     path('salon/<int:salon_id>', views.salon, name='salon'),
     path('salon/<int:salon_id>/service/<int:service_id>/', views.select_employee, name='select_employee'),
     path('salon/<int:salon_id>/service/<int:service_id>/employee/<int:employee_id>', views.select_date,name='select_date'),
-    path('salon/<int:salon_id>/service/<int:service_id>/employee/<int:employee_id>/date/<str:date>',views.select_hour,name='select_hour')
+    path('salon/<int:salon_id>/service/<int:service_id>/employee/<int:employee_id>/date/<str:date>',views.select_hour,name='select_hour'),
+    path('salon/<int:salon_id>/service/<int:service_id>/employee/<int:employee_id>/date/<str:date>/hour/<str:hour>',
+         views.add_appointment, name='add-appointment'),
+    path('appointments', views.my_appointments, name='appointments'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    # In Django 5, LogoutView has been deprecated
+    path('logout/', views.logout_page, name='logout'),
+    path('cancel_appointment/<int:appointment_id>/',views.cancel_appointment,name='cancel_appointment'),
+    path('review/<int:appointment_id>/', views.review, name='review')
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- beautysalon/appointments/views.py
+++ beautysalon/appointments/views.py
@@ -1,36 +1,116 @@
-from django.shortcuts import render
-from .models import Salon, Service, Employee, Appointment
-
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from .models import Salon, Service, Employee, Appointment,Review
+from django.contrib.auth.models import User
+from django.contrib.auth import logout
+from .forms import ReviewForm
 
 def homepage(request):
     salons = Salon.objects.all()
     return render(request, 'homepage.html', {'salons': salons})
 
-def salon(request,salon_id):
+
+def salon(request, salon_id):
     salon_info = Salon.objects.get(pk=salon_id)
     services = Service.objects.filter(salon=salon_id).all()
-    return render(request, 'salon.html', {'salon':salon_info,'services':services})
 
-def select_employee(request,salon_id,service_id):
+
+    return render(request, "salon.html",
+                  {'salon': salon_info, 'services': services})
+
+
+def select_employee(request, salon_id, service_id):
     salon_info = Salon.objects.get(pk=salon_id)
     service = Service.objects.get(pk=service_id)
     employees = Employee.objects.filter(salon=salon_id).all()
-    return render(request,'select_employee.html', {'salon': salon_info,'service': service,'employees': employees})
 
-def select_date(request,salon_id,service_id,employee_id):
+    return render(request, "select_employee.html",
+                  {'salon': salon_info, 'service': service, 'employees': employees})
+
+
+def select_date(request, salon_id, service_id, employee_id):
     salon_info = Salon.objects.get(pk=salon_id)
     service = Service.objects.get(pk=service_id)
     employee = Employee.objects.get(pk=employee_id)
-    return render(request,'select_date.html',{'salon': salon_info,'service': service,'employee':employee})
 
-def select_hour(request,salon_id,service_id,employee_id,date):
-    time_slots = range(10,18)
+    return render(request, "select_date.html",
+                  {"salon": salon_info, "service": service, "employee": employee})
+
+
+def select_hour(request, salon_id, service_id, employee_id, date):
+    # All employees work from 10:00 AM to 18:00 PM
+    time_slots = range(10, 18)
+
     salon_info = Salon.objects.get(pk=salon_id)
     service = Service.objects.get(pk=service_id)
     employee = Employee.objects.get(pk=employee_id)
-    # appointments = Appointment.objects.filter(salon=salon_id,employee=employee_id)
-    return render(request,'select_hour.html',{'salon':salon_info,'service':service,'employee':employee,'time_slots':time_slots,'date':date})
+
+    # TODO: Get all the appointments for this employee this day
+    #  and compile a list of available timeslots
+    # appointments = Appointment.objects.filter(salon=salon_id, employee=employee_id
+
+    return render(request, "select_hour.html",
+                  {'salon': salon_info, 'service': service, 'employee': employee,
+                   'time_slots': time_slots, 'date': date})
+
+
+def add_appointment(request, salon_id, service_id, employee_id, date, hour):
+    # Fetched the data from database so give it to the
+    # template to render
+    salon_info = Salon.objects.get(pk=salon_id)
+    service = Service.objects.get(pk=service_id)
+    employee = Employee.objects.get(pk=employee_id)
+    client = User.objects.get(pk=request.user.id)
+
+    # Create an Appointment object based on the arguments
+    appointment = Appointment(date=date, hour=hour,
+                              employee=employee,
+                              salon=salon_info,
+                              service=service,
+                              client=client)
+    # Save the Appointment object into the database
+    appointment.save()
+
+    # Render the appointment success template
+    return render(request, 'appointment_success.html', {
+        'salon': salon_info, 'service': service, 'employee': employee,
+        'date': date, 'hour': hour
+    })
+
+
+def my_appointments(request):
+    # Fetch all the appointments from the database
+    # where the client is the user that is requesting it
+    appointments = Appointment.objects.filter(client=request.user.id).all()
+
+    return render(request, 'my_appointments.html', {'appointments': appointments})
+
+
+def logout_page(request):
+    logout(request)
+    return redirect('homepage')
+
+def cancel_appointment(request,appointment_id):
+    appointment = Appointment.objects.get(id=appointment_id)
+    if request.method == 'POST':
+        appointment.delete()
+        return redirect('appointments')
+
+    return render(request, 'cancel_appointment.html', {'appointment': appointment})
+
+
+def review(request, appointment_id):
+    appointment = Appointment.objects.get(id=appointment_id)
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.appointment = appointment
+            review.client = request.user
+            return redirect('salon', pk=appointment.salon.id)
+    else:
+        form = ReviewForm()
+
+    return render(request, 'review.html', {'form': form, 'appointment': appointment})
 
 
